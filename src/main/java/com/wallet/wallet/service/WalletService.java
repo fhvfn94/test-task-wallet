559f0898df3aa2cb3488a6b7eaefedffbe0484b2
IshmukhanbetovEK <fhvfn94@gmail.com>
2024-02-27 14:11:54 +0300

    updateWallet changed

--- src/main/java/com/wallet/wallet/service/WalletService.java
+++ src/main/java/com/wallet/wallet/service/WalletService.java
@@ -23,7 +23,7 @@ public class WalletService {
         BigDecimal amount = walletDto.getAmount();
         if (amount.compareTo(BigDecimal.ZERO) <= 0) {
             throw new InvalidRequestException("amount <= 0");
-        } else if (!operationType.equals("DEPOSIT") && !operationType.equals("WITHDRAW")) {
+        } else if (!operationType.equals("DEPOSIT")) {
             throw new IllegalArgumentException("Invalid operationType: " + operationType);
         } else {
             WalletEntity wallet = new WalletEntity();
@@ -34,14 +34,35 @@ public class WalletService {
         }
     }
 
-    public WalletEntity updateWallet(UUID walletId, WalletDto walletDto) {
-        WalletEntity wallet = getWalletById(walletId);
-        wallet.setOperationType(walletDto.getOperationType());
-        wallet.setAmount(walletDto.getAmount());
-        walletRepository.save(wallet);
-        return wallet;
+    private void validateRequest(String operationType, BigDecimal amount) {
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new InvalidRequestException("amount <= 0");
+        } else if (!operationType.equals("DEPOSIT") && !operationType.equals("WITHDRAW")) {
+            throw new IllegalArgumentException("Invalid operationType: " + operationType);
+        }
     }
+    public WalletEntity updateWallet(UUID walletId, WalletDto walletDto) {
+        if (walletId != null && walletRepository.existsById(walletId)) {
 
+            String operationType = walletDto.getOperationType();
+            BigDecimal amount = walletDto.getAmount();
+
+            validateRequest(operationType, amount);
+
+            WalletEntity wallet = getWalletById(walletId);
+
+            if (operationType.equals("DEPOSIT")) {
+                wallet.setOperationType(operationType);
+                wallet.setAmount(wallet.getAmount().add(amount));
+            } else if (operationType.equals("WITHDRAW")) {
+                wallet.setOperationType(operationType);
+                wallet.setAmount(wallet.getAmount().subtract(amount));
+            }
+            return walletRepository.save(wallet);
+        } else {
+            throw new RuntimeException(walletId + "did not fined");
+        }
+    }
 
     public WalletEntity getWalletById(UUID walletId) {
         return walletRepository.findById(walletId).get();
